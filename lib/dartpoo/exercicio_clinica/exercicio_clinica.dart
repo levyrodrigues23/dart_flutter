67cf13bb15dd1b01cc40b2b3dc71ba153f7840c9
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2025-12-20 00:21:07 -0300

    refactor: improve Paciente and Consulta classes with copyWith methods and final fields

--- lib/dartpoo/exercicio_clinica/exercicio_clinica.dart
+++ lib/dartpoo/exercicio_clinica/exercicio_clinica.dart
@@ -3,55 +3,71 @@
 // consulta deve ter dois métodos importantes: remarcar consulta e exibir detalhes da consulta.
 
 
-import 'dart:io';
+
+
+
 
 class Paciente{
-  String nome;
-  int idade;
-  String telefone;
+  final String nome;
+  final int idade;
+  final String telefone;
 
 
 Paciente(this.nome, this.idade, this.telefone);
 
-void atualizarTelefone(String telefoneNovo){
-telefone = telefoneNovo;
+Paciente atualizarTelefone(String telefone){
+  return copyWith(telefone: telefone);
 }
 
 void exibirDados(){
   print("Paciente: $nome, Idade: $idade, Telefone: $telefone");
 }
 
+@override
+  String toString() {
+    return "Paciente: $nome, Idade: $idade, Telefone: $telefone";
+  }
+
+
+Paciente copyWith({String? nome, int? idade, String? telefone}){
+return Paciente(nome ?? this.nome, idade ?? this.idade, telefone ?? this.telefone);
+}
 }
 
 class Consulta{
-  Paciente paciente;
-  DateTime data;
-  String especialidade;
+  final Paciente paciente;
+  final   DateTime data;
+  final String especialidade;
 
 
-  Consulta(this.paciente, this.especialidade) : data = DateTime.now();
+  Consulta(this.paciente, this.especialidade, this.data);
 
-  void remarcarConsulta(DateTime novaData){
-    data = novaData;
+  Consulta remarcarConsulta(DateTime novaData){
+    return copyWith(data: novaData);
 
   }
 
   void exibirDetalhesDaConsulta(){
-    print("Data da Consulta: $data, especialidade: $especialidade ");
-    paciente.exibirDados();
+    print("Nome: {$paciente}, Data da Consulta: $data, especialidade: $especialidade ");
   }
 
 
+Consulta copyWith({Paciente? paciente, DateTime? data, String? especialidade}){
+  return Consulta(paciente ?? this.paciente, especialidade ?? this.especialidade, data ?? this.data);
+}
 
 }
 
 void main(){
   final primeiroPaciente = Paciente('Lara', 15, '85996221442');
   final segundoPaciente = Paciente('Levy', 19, '85999808147');
+  primeiroPaciente.exibirDados();
+  final pacienteAlterado = primeiroPaciente.atualizarTelefone('402228922');
+  pacienteAlterado.exibirDados();
 
-final consulta = Consulta(primeiroPaciente, 'doença cardiovascular');
+final consulta = Consulta(primeiroPaciente, 'doença cardiovascular', DateTime(2024, 11, 15));
 consulta.exibirDetalhesDaConsulta();
-final consulta2 = Consulta(segundoPaciente, 'doença de chagas');
+final consulta2 = Consulta(segundoPaciente, 'doença de chagas', DateTime(2024, 12, 20));
 (consulta2.exibirDetalhesDaConsulta());
 consulta2.remarcarConsulta(DateTime(2025));
 consulta2.exibirDetalhesDaConsulta();
